8ce649fcbfc7352b74628d66b505b31d2fd2b50a
efratzadok0879 <efrat.zadok@seldatinc.com>
2018-09-13 15:25:56 +0300

    efrat

--- 13.09.2018/node server + mongo db/controllers/user.js
+++ 13.09.2018/node server + mongo db/controllers/user.js
@@ -1,13 +1,43 @@
 const models = require('../models/index');
-
+const jwt = require('jsonwebtoken');
+const User = require('./../models/index').User;
 function addUserRoutes(app) {
-    //Get all countries
+
     app.get('/users', function (request, respones) {
-        models.User.find({},(err,users)=>{
+        models.User.find({}, (err, users) => {
             respones.send(users);
         });
     });
-}
 
+    app.post('/register', function (request, respones) {
+        let user = new User(request.body);
+        console.log(user);
+        user.save((err, res) => {
+            console.log(err || "Insert user succsess");
+            if (!err) {
+                //create token
+                let token = jwt.sign(user, 'my secret');
+                console.log(token);
+                res.send(token);
+            }
+        });
+
+    })
+
+    app.post('/login', function (request, respones) {
+        let username = request.body.username;
+        let password = request.body.password;
+        models.User.findOne({ name: username, password: password }, (err) => {
+            if (!err) {
+                //create token
+                let token = jwt.sign(request.body, 'my secret');
+                console.log(token);
+                respones.send(token);
+            }
+
+        });
+
+    });
+}
 
 module.exports = { addUserRoutes };
--- 13.09.2018/node server + mongo db/models/user.js
+++ 13.09.2018/node server + mongo db/models/user.js
@@ -8,3 +8,6 @@ module.exports = {
         visitedCountries: Array
     }
 };
+//  curl -v -X POST -H "Content-type: application/json" -d "{\"id\":\"207138132\", \"name\":\"Efrat\", \"password\":\"03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4\", \"age\":\"20\", \"isMale\":\"false\", \"visitedCountries\":\"[]\"}" http://localhost:3000/register
+
+ //  curl -v -X POST -H "Content-type: application/json" -d "{\"name\":\"Efrat\", \"password\":\"03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4\"}" http://localhost:3000/login
